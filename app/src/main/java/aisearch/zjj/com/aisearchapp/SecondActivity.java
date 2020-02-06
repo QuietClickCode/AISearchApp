f04bd0fbccb395fc4540ddc29a8dd0da82d46a65
QuietClickCode <2572417548@qq.com>
2020-02-06 21:20:22 +0800

    实现搜索显示列表内容了,还要自己写适配器来展示其他 的 字段

--- app/src/main/java/aisearch/zjj/com/aisearchapp/SecondActivity.java
+++ app/src/main/java/aisearch/zjj/com/aisearchapp/SecondActivity.java
@@ -3,16 +3,24 @@ package aisearch.zjj.com.aisearchapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class SecondActivity extends AppCompatActivity {
-    TextView responseText;
 
     String keyword;
+
+    ListView responseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,8 @@ public class SecondActivity extends AppCompatActivity {
         Bundle data = intent.getBundleExtra("data");
         keyword = (String) data.get("keyword");
         sendRequestWithOkHttp(keyword);
-        responseText = findViewById(R.id.response_text);
+
+
     }
 
     private void sendRequestWithOkHttp(String keyword) {
@@ -43,11 +52,43 @@ public class SecondActivity extends AppCompatActivity {
         }).start();
     }
 
+    //这儿转换好复杂,还是js直接点点点过去好
     private void showResponse(final String response) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                responseText.setText(response);
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = (JSONObject) JSONObject.parse(response);
+                    jsonObject = (JSONObject) jsonObject.get("data");
+                    JSONArray list = jsonObject.getJSONArray("list");
+                    Object[] objects = list.toArray();
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    for (Object s : objects) {
+                        arrayList.add(s.toString());
+                    }
+
+                    int size = arrayList.size();
+
+                    String[] strings = arrayList.toArray(new String[size]);
+                    ArrayList<String> arrayListTitle = new ArrayList<>();
+                    JSONObject jsonObject1;
+                    for (String s : strings) {
+                        jsonObject1 = (JSONObject) JSONObject.parse(s);
+                        String title = (String) jsonObject1.get("content");
+                        arrayListTitle.add(title);
+                    }
+                    int sizeTitle = arrayListTitle.size();
+
+                    String[] stringsTitle = arrayListTitle.toArray(new String[sizeTitle]);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(SecondActivity.this, android.R.layout.simple_list_item_1, stringsTitle);
+                    responseText = findViewById(R.id.response_text);
+                    responseText.setAdapter(adapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                /*responseText.setText(response);*/
                 /*responseText.setText(keyword);*/
             }
         });
